a3b33ab2c98fbb0b441992917311a66d180f4f15
JohnnyChen(Mac) <jc.freelance.2011@gmail.com>
2023-10-06 16:25:39 +0800

    設定build 1. 目標資料夾"outDir"：設定到dotnet mvc的wwwroot資料夾 2. rollupOptions設定不同資料夾的發佈路徑對應

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+/* init config */
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [vue()],
+// })
+/* init config */
+
+import { resolve } from 'path'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
 export default defineConfig({
   plugins: [vue()],
-})
+  build: {
+    outDir: '../wwwroot', // Specify the output directory here
+    minify: false,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        // nested: resolve(__dirname, 'nested/index.html'),
+      },
+    },
+  },
+});
