3c03fdc32884fd9146372211c3621d2759d8671c
JohnnyChen(Mac) <jc.freelance.2011@gmail.com>
2023-10-11 16:47:26 +0800

    npm install n setup SweetAlert2

---
+++ test-pool-basic/src/page-sweetalert-demo.js
@@ -0,0 +1,15 @@
+import { createApp } from 'vue'
+
+// Import our custom CSS
+import '../../src/scss/styles.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+
+
+//import './style.css'
+import App from './components/sweetalert-demo-app.vue'
+import './helpers'
+
+createApp(App).mount('#app')
